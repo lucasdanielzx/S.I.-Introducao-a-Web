6246f9d7d101f1d8e3b9020629e188e86b0c07e7
Lucas Daniel <101567841+lucasdanielzx@users.noreply.github.com>
2023-05-15 22:27:40 -0300

    Add files via upload

---
+++ SITE_IPHONE/js/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("#banners ul").bxSlider({
+        auto: true, //passar automaticamente
+        //mode: 'fade' //modo de transição
+        //pager: false //ocultar a paginação
+    });
+});  
